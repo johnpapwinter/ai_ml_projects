de4074fd53f7250391735e465c1666f956f296eb
johnpapwinter <johnpapahimonas@yahoo.com>
2024-04-04 14:24:48 +0300

    minor refactoring

--- bank_churn_classifier/data_preprocessing_pipeline.py
+++ bank_churn_classifier/data_preprocessing_pipeline.py
@@ -7,20 +7,20 @@ class DataPreprocessingPipeline:
         self.categorical_columns = categorical_columns
 
     def preprocess_data(self, dataset: pd.DataFrame) -> pd.DataFrame:
-        dataset = self.__drop_columns(dataset)
-        dataset = self.__one_hot_encoding(dataset)
-        dataset = self.__handle_bank_balance(dataset)
+        dataset = self._drop_columns(dataset)
+        dataset = self._one_hot_encoding(dataset)
+        dataset = self._handle_bank_balance(dataset)
         return dataset
 
-    def __one_hot_encoding(self, dataset: pd.DataFrame) -> pd.DataFrame:
+    def _one_hot_encoding(self, dataset: pd.DataFrame) -> pd.DataFrame:
         dataset = pd.get_dummies(dataset, columns=self.categorical_columns)
         dataset.replace({True: 1, False: 0}, inplace=True)
         return dataset
 
-    def __drop_columns(self, dataset: pd.DataFrame) -> pd.DataFrame:
+    def _drop_columns(self, dataset: pd.DataFrame) -> pd.DataFrame:
         dataset.drop(columns=self.drop_columns, inplace=True)
         return dataset
 
-    def __handle_bank_balance(self, dataset: pd.DataFrame) -> pd.DataFrame:
+    def _handle_bank_balance(self, dataset: pd.DataFrame) -> pd.DataFrame:
         dataset['Balance'] = dataset['Balance'].apply(lambda x: 1 if x != 0 else 0)
         return dataset
